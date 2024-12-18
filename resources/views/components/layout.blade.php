94ee827b060874002a2d573167201b5ea9b18eb1
alex <alex@alexsykes.net>
2024-12-18 20:07:31 +0300

    Site update added and working

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -32,7 +32,7 @@ echo env('APP_NAME');
 
   <header class="bg-lime-100 shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
     </div>
   </header>
   <main>
--- resources/views/sitedetail.blade.php
+++ resources/views/sitedetail.blade.php
@@ -1,8 +1,11 @@
 <x-layout>
-<x-slot:heading>{{ $site->site_name }}</x-slot:heading>
+	<div class="flex justify-between">
+		<x-slot:heading>{{ $site->site_name }}</x-slot:heading>
+		<x-button href="/sites/{{ $site->id }}/edit">Edit Site</x-button>
+	</div>
 	<div>{{ $site->site_description }}</div>
 	<div><strong>Access:</strong> {{$site->site_access }}</div>
 	<div><strong>Wind(s): {{$site->site_wind_directions }}</strong></div>
 
-	<x-button href="/sites/{{ $site->id }}/edit">Edit Site</x-button>
+
 </x-layout>
