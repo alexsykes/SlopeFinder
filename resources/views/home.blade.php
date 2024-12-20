e3095b542da19ea35cc3911b05d3de98dc1ce292
Alex Sykes <alex@alexsykes.net>
2024-12-20 10:04:02 +0300

    Update home.blade.php
    Removed sensitive data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
 <x-slot:heading>SlopeFinder</x-slot:heading>
 	<script>
 		(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-			key: "AIzaSyA4MBz6Xce0OZvEZ-vinOUCqEw8oHsZTsg",
+			key: "-",
 			v: "weekly",
 			// Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
 			// Add other bootstrap parameters as needed, using camel case.
@@ -108,7 +108,7 @@
 						});
 			}
 			else {
-				httpGetAsync("https://api.openweathermap.org/data/3.0/onecall?lat=53.8&lon=-2&units=metric&exclude=minutely&appid=6aff05a2599912b387c2b5390360cc84",myCallback)
+				httpGetAsync("https://api.openweathermap.org/data/3.0/onecall?lat=53.8&lon=-2&units=metric&exclude=minutely&appid=",myCallback)
 			}
 		}
 		function httpGetAsync(theUrl, callback)
